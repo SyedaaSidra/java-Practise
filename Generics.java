541f254c8e64f0494ff48470db0b5e379e3c983a
sidraRashdi <rashdimuntaha557@gmail.com>
2025-06-09 15:58:30 +0500

    Create a java Class for generics practice.

---
+++ Generics.java
@@ -0,0 +1,19 @@
+public class Generics<T> {
+    T PrinterVal;
+   Generics(T PrinterVal){
+        this.PrinterVal=PrinterVal;
+      }
+  public T print(){
+    return PrinterVal;
+    
+  }
+    public static void main (String[] args){
+       Generics<Integer> printer=new Generics<>(23);
+         System.out.println(printer.print());
+
+         Generics<String> Strprinter=new Generics<>("hey");
+         System.out.println(Strprinter.print());
+       
+       
+    }
+}
